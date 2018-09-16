2010b475ef0c591a095442e916d15f5bfad0a5f5
Adarshi Sinha <adarshisinha@gmail.com>
2018-09-16 22:09:15 +0530

    Ass4

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -10,7 +10,7 @@ namespace Transform
         public static int[] Map(this int[] array, ExpressionDelegate expressionDelegate)
         {
                 int[] result=new int[array.Length];
-                // logic
+                // for each number, using delegate, return its mapped value
                 for(int i=0; i< array.Length; i++)
                 {
                     result[i]=expressionDelegate(array[i]);
